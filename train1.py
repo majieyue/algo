1aa547e21db8341a5d309d8555ba1cdf79decbeb
Ma Jie Yue <majieyue@gmail.com>
2025-05-06 21:08:28 +0800

    update

--- train1.py
+++ train1.py
@@ -10,12 +10,18 @@ os.environ["MASTER_PORT"] = "29505"
 os.environ["RANK"] = "0"
 os.environ["WORLD_SIZE"] = "1"
 
+if not torch.cuda.is_available():
+    raise RuntimeError("CUDA is not available.")
+
+device=torch.device(f'cuda:0')
+torch.cuda.set_device(0)
 dist.init_process_group(backend="nccl", rank=0, world_size=1)
 
 rank=dist.get_rank()
 world_size=dist.get_world_size()
 print(f"Rank: {rank}, World size: {world_size}")
 
-time.sleep(30)
+time.sleep(5)
 
+dist.barrier(device_ids=[device.index])
 dist.destroy_process_group()
